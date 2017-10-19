cf5754ae053aad11d7a025be48fec3e78fe1df8b
Marhold Mônier <marhold.monier@gmail.com>
2017-10-19 10:33:41 +0200

    Little debug

--- courbe_tjrs/controls.js
+++ courbe_tjrs/controls.js
@@ -1,21 +1,23 @@
 class Controls {
-    constructor(game, player, Phaser) {
+    constructor(game, player, texture, Phaser, cursors) {
         this.game = game;
+        this.texture = texture;
         this.player = player;
-        this.Phaser = Phaser
+        this.Phaser = Phaser;
+        this.cursors = cursors;
     }
 
     mouse_control() {
-        if (game.input.mousePointer.isDown) {
+        if (this.game.input.mousePointer.isDown) {
             //  400 is the speed it will move towards the mouse
-            game.physics.arcade.moveToPointer(player, 400);
+            this.game.physics.arcade.moveToPointer(this.player, 400);
 
             //  if it's overlapping the mouse, don't move any more
-            if (Phaser.Rectangle.contains(player.body, game.input.x, game.input.y)) {
-                player.body.velocity.setTo(0, 0);
+            if (this.Phaser.Rectangle.contains(this.player.body, this.game.input.x, this.game.input.y)) {
+                this.player.body.velocity.setTo(0, 0);
             }
         } else {
-            player.body.velocity.setTo(0, 0);
+            this.player.body.velocity.setTo(0, 0);
         }
     }
 
@@ -24,48 +26,46 @@ class Controls {
         //  If the cursor is locked to the game, and the callback was not fired from a 'click' event
         //  (such as a mouse click or touch down) - as then it might contain incorrect movement values
         while (click) {
-            player.x += game.input.mouse.event.movementX;
-            player.y += game.input.mouse.event.movementY;
+            this.player.x += this.game.input.mouse.event.movementX;
+            this.player.y += this.game.input.mouse.event.movementY;
         }
     }
 
     keyboard_control2() {
-        texture.renderXY(player, player.x, player.y);
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+        this.player.body.angularVelocity = 0;
 
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
-        player.body.angularVelocity = 0;
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            player.body.angularVelocity = -10;
-            player.body.angularAcceleration -= 10;
-        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            player.body.angularVelocity = 10;
-            player.body.angularAcceleration += 10;
+        if (this.game.input.keyboard.isDown(this.Phaser.Keyboard.LEFT)) {
+            this.player.body.angularVelocity = -10;
+            this.player.body.angularAcceleration -= 10;
+        } else if (this.game.input.keyboard.isDown(this.Phaser.Keyboard.RIGHT)) {
+            this.player.body.angularVelocity = 10;
+            this.player.body.angularAcceleration += 10;
         }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            game.physics.arcade.velocityFromAngle(player.angle, 100, player.body.velocity);
+        if (this.game.input.keyboard.isDown(this.Phaser.Keyboard.UP)) {
+            this.game.physics.arcade.velocityFromAngle(this.player.angle, 100, this.player.body.velocity);
         }
     }
 
     keyboard_control() {
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
 
-        if (cursors.left.isDown)
-            player.body.velocity.x = -150;
+        if (this.cursors.left.isDown)
+            this.player.body.velocity.x = -150;
 
-        if (cursors.right.isDown)
-            player.body.velocity.x = 150;
+        if (this.cursors.right.isDown)
+            this.player.body.velocity.x = 150;
 
-        if (cursors.up.isDown)
-            player.body.velocity.y = -150;
+        if (this.cursors.up.isDown)
+            this.player.body.velocity.y = -150;
 
-        if (cursors.down.isDown)
-            player.body.velocity.y = 150;
+        if (this.cursors.down.isDown)
+            this.player.body.velocity.y = 150;
     }
 
-
+    /*
     gamepad_control() {
         // Pad "connected or not" indicator
         if (game.input.gamepad.supported && game.input.gamepad.active && pad1.connected)
@@ -103,4 +103,5 @@ class Controls {
                 sprite.y += rightStickY * 10;
         }
     }
+    */
 }
--- courbe_tjrs/main.js
+++ courbe_tjrs/main.js
@@ -5,10 +5,14 @@ window.onload = function() {
     //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
     const game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+    const gameWidth = 600;
+    const gameHeight = 600;
+
     let player;
     let cursors;
     let randomCol = getRandomColor();
     let texture;
+    let controls;
 
     function preload() {
         // si on a besoin de charger des images
@@ -16,7 +20,7 @@ window.onload = function() {
 
     function create() {
         let circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 15);
-        texture = game.add.renderTexture(600, 600, 'mousetrail');
+        texture = game.add.renderTexture(gameWidth, gameHeight, 'mousetrail');
         // création de la boule
         player = game.add.graphics(0, 0);
         player.beginFill(randomCol, 1);
@@ -38,29 +42,35 @@ window.onload = function() {
         cursors = game.input.keyboard.createCursorKeys();
         // game.input.addMoveCallback(move, this); 
 
-        controls = Controls(game, player, Phaser);
+        controls = new Controls(game, player, texture, Phaser, cursors);
+
     }
 
     function render() {}
 
     function update() {
+        texture.renderXY(player, player.x, player.y);
+        addBomb();
         //  Reset the players velocity (movement)
         // controls claviers
         //keyboard_control();       
         // console.log(player.x, player.y);
         //  only move when you click
-        keyboard_control2();
+        controls.keyboard_control();
     }
 
     function playerOut() {
         // pour voir ou est le joueur
         console.log(player.x, player.y);
-        // pour téléporter le joueur de l'autre coté de l'écran si il sort
-        if (Math.abs(player.x) > 300)
-            player.reset(player.x * -1, player.y);
 
-        if (Math.abs(player.y) > 300)
+        // pour téléporter le joueur de l'autre coté de l'écran si il sort
+        if (Math.abs(player.x) > gameWidth / 2) {
+            player.reset(player.x * -1, player.y);
+        }
+
+        if (Math.abs(player.y) > gameHeight / 2) {
             player.reset(player.x, player.y * -1);
+        }
     }
 
     function getRandomColor() {
@@ -69,5 +79,16 @@ window.onload = function() {
         let hex = '0x';
         while (length--) hex += chars[(Math.random() * 16) | 0];
         return hex;
+    }
+
+    function isLost() {
+
+    }
+
+    function addBomb() {
+        if (Math.random() > .991 /*.995*/) {
+            let bomb = new Phaser.Circle(10, 10, 15);  
+            console.debug("bomb");
+        }
     }
 };
